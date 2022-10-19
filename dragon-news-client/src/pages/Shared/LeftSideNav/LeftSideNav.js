6e77d643e9e9b0e2b7c036e98499e30b230eeaed
Sanjit Sarkar <sanjit.est14@gmail.com>
2022-10-19 12:02:14 +0600

    ReInitial commit

---
+++ dragon-news-client/src/pages/Shared/LeftSideNav/LeftSideNav.js
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+
+const LeftSideNav = () => {
+  const [categories, setCategories] = useState([]);
+
+  return (
+    <div>
+      <h4>All Category</h4>
+    </div>
+  );
+};
+
+export default LeftSideNav;
